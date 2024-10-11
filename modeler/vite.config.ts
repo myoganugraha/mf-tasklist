595fa4df7b3e6d0c4d2757a531e127cad49cdc72
M. Yoga Nugraha <myoganugraha@gmail.com>
2024-10-11 09:58:06 +0700

    chore: add modeler app

---
+++ modeler/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import federation from "@originjs/vite-plugin-federation";
+
+const configuration = {
+  APP_NAME: "modeler",
+  APP_FILE_NAME: "remoteEntry.js",
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: configuration.APP_FILE_NAME,
+      filename: configuration.APP_FILE_NAME,
+      exposes: {
+        "./Tasklist": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    target: "esnext",
+  },
+});
