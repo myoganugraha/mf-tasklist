a27ef9b736fc60d820a55b8023ecd9107651e550
M. Yoga Nugraha <myoganugraha@gmail.com>
2024-10-16 14:33:07 +0700

    feat: implement login flow on the ui

--- login/src/LoginPage.tsx
+++ login/src/LoginPage.tsx
@@ -1,12 +1,43 @@
-import { Button, Form, Input } from "antd";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button, Form, notification, Input } from "antd";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./Login.css";
+import { loginApi } from "./service/login.service";
+import { useState } from "react";
+
+type NotificationType = "success" | "info" | "warning" | "error";
 
 const LoginPage = () => {
+  const { postLogin } = loginApi();
+  const [api, contextHolder] = notification.useNotification();
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  const onFinish = async (values: any) => {
+    setLoading(true);
+    try {
+      await postLogin(values.username, values.password);
+    } catch (e: any) {
+      openNotification("error", "Login Failed", e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const openNotification = (
+    type: NotificationType,
+    title: string,
+    description: string
+  ) => {
+    api[type]({
+      message: title,
+      description: description,
+    });
+  };
+
   return (
     <>
-    <div>
+      {contextHolder}
+      <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,18 +47,21 @@ const LoginPage = () => {
       </div>
       <Form
         autoComplete="off"
+        onFinish={onFinish}
         style={{
           minWidth: 360,
         }}
       >
-        <Form.Item name="email">
-          <Input placeholder="E-mail" />
+        <Form.Item name="username" rules={[{ required: true }]}>
+          <Input placeholder="Username" />
         </Form.Item>
-        <Form.Item name="password">
+        <Form.Item name="password" rules={[{ required: true }]}>
           <Input.Password placeholder="Password" />
         </Form.Item>
         <Form.Item>
-          <Button block>Log In</Button>
+          <Button block htmlType="submit" loading={isLoading}>
+            Log In
+          </Button>
         </Form.Item>
       </Form>
     </>
@@ -35,4 +69,3 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
-
