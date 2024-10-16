56dca7ac5c63ef2d28fd8383f98e04440d3e27af
M. Yoga Nugraha <myoganugraha@gmail.com>
2024-10-16 15:55:31 +0700

    feat: add store provider on root apps

--- login/src/main.tsx
+++ login/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import LoginPage from './LoginPage.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import LoginPage from "./LoginPage.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <LoginPage />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <LoginPage />
+    </Provider>
+  </StrictMode>
+);
