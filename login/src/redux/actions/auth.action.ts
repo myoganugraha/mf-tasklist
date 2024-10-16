c49400578f709443173ed9a847dff5949ce1000a
M. Yoga Nugraha <myoganugraha@gmail.com>
2024-10-16 16:12:49 +0700

    feat: create userDetailsAction

--- login/src/redux/actions/auth.action.ts
+++ login/src/redux/actions/auth.action.ts
@@ -1,12 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { loginApi } from "../../service/login.service";
+import { userApi } from "../../service/user.service";
 
 const { postLogin } = loginApi();
+const { getUserDetails } = userApi();
 
 export const loginAction = createAsyncThunk(
   "/generate-jwt",
   async (data: any, thunkAPI) => {
     return await postLogin(data.username, data.password).catch((error) =>
+      thunkAPI.rejectWithValue(error?.response?.data || error)
+    );
+  }
+);
+
+export const userDetailsAction = createAsyncThunk(
+  "/account",
+  async (token: string, thunkAPI) => {
+    return await getUserDetails(token).catch((error) =>
       thunkAPI.rejectWithValue(error?.response?.data || error)
     );
   }
